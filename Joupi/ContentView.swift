a12ee3ca183a78a4fbf8bb6e058ecc6b4a8ab8e0
EddoCIP <eddocareera.i.p@gmail.com>
2022-05-11 13:49:01 +0700

    Initial Commit

---
+++ Joupi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Joupi
+//
+//  Created by Eddo Careera Iriyanto Putra on 11/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Joupi/JoupiApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoupiApp.swift
+//  Joupi
+//
+//  Created by Eddo Careera Iriyanto Putra on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct JoupiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
