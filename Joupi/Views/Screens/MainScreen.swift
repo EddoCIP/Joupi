8eb51d1626ef7c2a90baaf46bcddd4f0ba68e5d9
EddoCIP <eddocareera.i.p@gmail.com>
2022-05-11 22:17:10 +0700

    testing mainscreen

--- Joupi/Views/Screens/MainScreen.swift
+++ Joupi/Views/Screens/MainScreen.swift
@@ -8,8 +8,73 @@
 import SwiftUI
 
 struct MainScreen: View {
+    @State var scrollOffset: CGFloat = .zero
+    private static let initialColumns = 2
+    @State private var gridColumns = Array(repeating: GridItem(.flexible()), count: initialColumns)
+    @State private var numColumns = initialColumns
+    let screenHeight = UIScreen.main.bounds.height
+    let screenWidth = UIScreen.main.bounds.width
+    
+    private var hasScrolled: Bool {
+        scrollOffset > 0
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                if !hasScrolled {
+                    Rectangle()
+                        .foregroundColor(.white)
+                        .frame(width: screenWidth, height: 200)
+                        .overlay {
+                            Text("Hi Coffee enthusiast")
+                                .bold()
+                        }
+                } else {
+                    Text("My Journal Ngopi")
+                        .bold()
+                }
+                
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.brown)
+                        .frame(width: screenWidth, height: screenHeight)
+                        .cornerRadius(10)
+                    VStack {
+                        ObservableScrollView(scrollOffset: $scrollOffset) { _ in
+                            LazyVGrid(columns: gridColumns) {
+                                
+                            }
+                        }
+                        Button("Create Journal") {
+                            
+                        }
+                    }
+                }
+            }
+            //            ScrollView {
+            //                VStack {
+            //                    Text("Hi Coffee enthusiast")
+            //                    ZStack {
+            //                        Rectangle()
+            //                            .foregroundColor(.brown)
+            //                            .ignoresSafeArea()
+            //                        VStack {
+            //                            Text("My Journal Ngopi")
+            //                            Spacer()
+            //                            Text("Your Journal is still empty.")
+            //                            Text("Let's create a new one!")
+            //                            Spacer()
+            //                            Button("Create Journal") {
+            //
+            //                            }
+            //                        }
+            //                    }
+            //                }
+            //                .navigationBarHidden(true)
+            //                .navigationViewStyle(StackNavigationViewStyle())
+            //            }
+        }
     }
 }
 
