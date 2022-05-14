d92ce6c354fdc0adbd71a29fe3854c7c872132ed
EddoCIP <eddocareera.i.p@gmail.com>
2022-05-14 14:43:16 +0700

    trial carousel

--- Joupi/Views/Shareds/ImageCarousel.swift
+++ Joupi/Views/Shareds/ImageCarousel.swift
@@ -8,8 +8,38 @@
 import SwiftUI
 
 struct ImageCarousel: View {
+    @State var index: Int = 0
+    
+    var additionalTab: () -> Content
+//    var views : [View] = []
+    
+//    init(v: View)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TabView(selection: $index) {
+                ForEach(0..<3, id: \.self) { index in
+                    ImageCard()
+                }
+                ImageCard()
+                    .tabItem {
+//                        Image(systemName: "2.square.fill")
+//                        Text("Second")
+                    }
+            }
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+        }
+        .frame(height: 200)
+    }
+}
+
+struct ImageCard: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color.pink)
+            .frame(height: 200)
+            .border(Color.black)
+            .padding()
     }
 }
 
