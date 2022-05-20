6a31a5adfd73d8642c765cb20b612392b2975537
rafiklutfi <dev.rafiklutfi@gmail.com>
2022-05-20 13:39:04 +0700

    rafik backup

---
+++ Joupi/Views/Screens/OnboardingView.swift
@@ -0,0 +1,87 @@
+//
+//  OnboardingView.swift
+//  Joupi
+//
+//  Created by Eddo Careera Iriyanto Putra on 19/05/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    @Binding var isStart: Bool
+    
+    var body: some View {
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            VStack {
+                VStack {
+                    Spacer()
+                    HStack(alignment: .bottom) {
+                        Spacer()
+                        Text("JOUPI")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("BackgroundColor"))
+                        Image(systemName: "cup.and.saucer")
+                            .resizable()
+                            .frame(width: 144, height: 109)
+                            .foregroundColor(Color("BackgroundColor"))
+                    }
+                }
+                .background(Color("PrimaryAccentColor"))
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.35)
+                VStack(alignment: .leading) {
+                    VStack(alignment: .leading) {
+                        Text("1. Journal")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("PrimaryAccentColor"))
+                        Text("Record your coffee experience journal by cup of coffee, beans and method.")
+                            .font(.body)
+                            .foregroundColor(Color("PrimaryAccentColor"))
+                    }.padding([.bottom, .leading, .trailing])
+                    VStack(alignment: .leading) {
+                        Text("2. Search & Sort")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("PrimaryAccentColor"))
+                        Text("Find the recorded coffee experience by journal title and two option of sort, by drink date and by name.")
+                            .font(.body)
+                            .foregroundColor(Color("PrimaryAccentColor"))
+                    }.padding([.bottom, .leading, .trailing])
+                    VStack(alignment: .leading) {
+                        Text("3. View")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("PrimaryAccentColor"))
+                        Text("View the recorded and detail of coffee experience journal.")
+                            .font(.body)
+                            .foregroundColor(Color("PrimaryAccentColor"))
+                    }.padding([.bottom, .leading, .trailing])
+                }.padding()
+                Spacer()
+            }
+            VStack {
+                Spacer()
+                Button("Get Started") {
+                    withAnimation {
+                        isStart.toggle()
+                    }
+                }
+                .frame(width: 200, height: 50)
+                .background(Color("PrimaryAccentColor"))
+                .foregroundColor(.white)
+                .cornerRadius(30)
+            }
+            .zIndex(1)
+            .padding()
+        }
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView(isStart: .constant(false))
+    }
+}
